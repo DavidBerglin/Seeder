1b79187593582f1d8300c46be21ec0511754d98f
Martin <martin_lenart@icloud.com>
2024-08-11 12:55:43 +0200

    update

--- .scripts/create_nuget.sh
+++ .scripts/create_nuget.sh
@@ -16,8 +16,16 @@ if [[ -z $1 ]]; then
 fi
 
 
-CSPROJ_FILE="../seedGenerator/seedGenerator.csproj"
-NUGET_File="../seedGenerator/Seido.Utilities.SeedGenerator.$1.nupkg"
+NUGET_NAME="SeedGenerator"
+NUGET_ID="Seido.Utilities.$NUGET_NAME"
+APPUSAGE_NAME="AppUsage"
+
+NUGET_CSPROJ_DIR="../$NUGET_NAME"
+NUGET_CSPROJ_FILE="$NUGET_CSPROJ_DIR/$NUGET_NAME.csproj"
+NUGET_File="$NUGET_CSPROJ_DIR/$NUGET_ID.$1.nupkg"
+
+APPUSAGE_CSPROJ_DIR="../$APPUSAGE_NAME"
+APPUSAGE_CSPROJ_FILE="$APPUSAGE_CSPROJ_DIR/$APPUSAGE_NAME.csproj"
 
 LOCAL_FEED="/Users/Martin/Development/seidoNugets"
 PUBLISH_FEED="https://pkgs.dev.azure.com/martinlenart/seidoNugets/_packaging/seidoNugets/nuget/v3/index.json"
@@ -25,22 +33,28 @@ PUBLISH_FEED="https://pkgs.dev.azure.com/martinlenart/seidoNugets/_packaging/sei
 # Property to modify
 VERSION="$1"
 
-# Use sed to modify the property value
+# Use sed to modify Version in the Nuget project
 #https://sed.js.org/#
 #https://stackoverflow.com/questions/45018156/using-sed-for-search-and-replace-multi-digits
-#sed -i '.bak' -r 's/(<Version>)[0-9]+[.][0-9]+[.][0-9]+(<\/Version>)/\1replace\2/' ../seedGenerator/seedGenerator.csproj1
-sed -i '.bak' -r "s/(<Version>)[0-9]+[.][0-9]+[.][0-9]+(<\/Version>)/\1$VERSION\2/" $CSPROJ_FILE
+#sed -r 's/(<Version>)[0-9]+[.][0-9]+[.][0-9]+(<\/Version>)/\1replace\2/' ../seedGenerator/seedGenerator.csproj
+sed -i '.bak' -r "s/(<Version>)[0-9]+[.][0-9]+[.][0-9]+(<\/Version>)/\1$VERSION\2/" $NUGET_CSPROJ_FILE
+echo "Updated version to $VERSION in $NUGET_CSPROJ_FILE"
 
-echo "Updated version to $VERSION in $CSPROJ_FILE"
+
+# Use sed to modify Version in the AppUsage project
+#https://superuser.com/questions/112834/how-to-match-whitespace-in-sed
+#sed -r "s/(<PackageReference[ \t]+Include=\"Seido[.]Utilities[.]SeedGenerator\"[ \t]+Version=\")[0-9]+[.][0-9]+[.][0-9]+(\"[ \t]*\/>)/\1replace\2/" ../AppUsage/AppUsage.csproj
+sed -i '.bak' -r "s/(<PackageReference[ \t]+Include=\"$NUGET_ID\"[ \t]+Version=\")[0-9]+[.][0-9]+[.][0-9]+(\"[ \t]*\/>)/\1$VERSION\2/" $APPUSAGE_CSPROJ_FILE
+echo "Updated version to $VERSION in $APPUSAGE_CSPROJ_FILE"
 
 #clear the local nuget cache
 dotnet nuget locals all --clear
 
 #build the nuget package project
-dotnet build $CSPROJ_FILE
+dotnet build $NUGET_CSPROJ_FILE
 
 #create the nuget package
-dotnet pack $CSPROJ_FILE -o ../seedGenerator
+dotnet pack $NUGET_CSPROJ_FILE -o $NUGET_CSPROJ_DIR
 
 #publish the nuget package to local feed
 dotnet nuget push $NUGET_File -s $LOCAL_FEED
